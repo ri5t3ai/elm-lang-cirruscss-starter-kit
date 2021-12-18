74b0db09e5a6a3f36519319a11e4f4f3318d5c1c
R15t3 <11156541+intelligenc3r@users.noreply.github.com>
2021-12-18 21:32:08 +0100

    SPA navigation bar added

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, text, h1, h2)
+import Html exposing (Html, button, div, text, h1, h2, img, span, i)
 import Html.Attributes exposing (class)
 import Html.Events exposing (onClick)
 
@@ -30,7 +30,61 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div [] [
-        div [ class "u-flex u-justify-center u-items-center h-screen" ]
+        div [ class "header header-fixed header-animated" ] [
+            div [ class "header-brand" ] [
+                div [ class "nav-item no-hover" ] [
+                      img [] []
+                    , span [ class "title" ] [text "site name"]
+                ]
+                , div [ class "nav-item nav-btn" ] [
+                      span [ class "title" ] [text "home"]
+                    , span [ class "title" ] [text "about"]
+                    , span [ class "title" ] [text "services"]
+                    , span [ class "title" ] [text "careers"]
+                    , span [ class "title" ] [text "contact"]
+                ]                
+                
+            ]
+            , div [ class "header-nav" ] [
+                div [ class "nav-left" ] [
+                    div [ class "nav-item" ] [
+                      span [ class " m-2" ] [text "home"]
+                    ]
+                    , div [ class "nav-item" ] [
+                      span [ class "m-2" ] [text "about"]
+                    ] 
+                    , div [ class "nav-item" ] [
+                      span [ class "s m-2" ] [text "services"]
+                    ] 
+                    , div [ class "nav-item" ] [
+                      span [ class " m-2" ] [text "careers"]
+                    ]
+                    , div [ class "nav-item" ] [
+                      span [ class " m-2" ] [text "blog"]
+                    ]
+                     , div [ class "nav-item" ] [
+                      span [ class "m-2" ] [text "contact"]
+                    ]          
+                ]
+          
+            ]
+                     , div [ class "header-nav" ] [
+                div [ class "nav-right" ] [
+                   
+                    
+                    div [ class "nav-item" ] [
+                       span [ class "icon " ] [
+                           i [class "fa-wrapper fa-github fab"] []
+                           , span [ class "title" ] [ text "login"]
+                       ]           
+                      
+                    ]
+                         
+                ]
+          
+            ]
+        ]
+        , div [ class "u-flex u-justify-center u-items-center h-screen" ]
             [ div [class "hero bg-indigo-600 w-90 h-50 u-justify-center u-items-center" ]
                 [ div [ class "hero-body u-flex-column "]
                     [ h1 [ class "headline-1 white" ] [ text "hello world"] 
